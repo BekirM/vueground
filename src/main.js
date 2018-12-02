0a504423aeea5b95ad529a3c4e10541328ec9024
Bekir Mamutov <bekir.mamutov@gmail.com>
2018-12-02 19:22:27 +0300

    New field

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import App from './App.vue'
 new Vue({
   el: '#app',
   data: {
-    message:"Initial value"
+    message:"Initial value",
+    checkList:[]s
+
+  },
+  methods:{
+
+  },
+  computed:{
+
   },
   render: h => h(App)
 })
